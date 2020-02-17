6b9b4f80d2b4e9ef061f9bdcda6eb855adb1f24f
John Kraus <john@johnfkraus.com>
2020-02-16 19:03:29 -0500

    added UnitOfMeasureReactiveRepository and play test

---
+++ src/main/java/guru/springframework/repositories/reactive/UnitOfMeasureReactiveRepository.java
@@ -0,0 +1,9 @@
+package guru.springframework.repositories.reactive;
+
+import guru.springframework.domain.UnitOfMeasure;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface UnitOfMeasureReactiveRepository extends ReactiveMongoRepository<UnitOfMeasure, String> {
+
+
+}
